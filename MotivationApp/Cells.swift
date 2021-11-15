6ac23c214342f2a54bea68fbf8721e3277de0eec
Nikita <nikita@MacBook-Air-Nikita.local>
2021-11-15 21:27:10 +0400

    Initial commit

---
+++ MotivationApp/Cells.swift
@@ -0,0 +1,14 @@
+//
+//  Cells.swift
+//  MotivationApp
+//
+//  Created by Nikita on 15.11.2021.
+//
+
+import UIKit
+
+class Cells: UITableViewCell {
+    
+
+    
+}
---
+++ MotivationApp/Model/Entity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Entity+CoreDataClass.swift
+//  MotivationApp
+//
+//  Created by Nikita on 15.11.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Entity: NSManagedObject {
+
+}
